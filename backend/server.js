b906d761ec1d52fd99c57aeea5ee66387c4cfa01
Naman Sharma <namansharma50962@gmail.com>
2024-06-14 22:42:20 +0530

    setting up enviornment

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-const express = require('express')
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const app = express();
+import express from 'express'
+import dotenv from 'dotenv'
+
+
+const app = express();
+dotenv.config();
+
+app.get('/',(req,res)=>{
+    res.send('Hellooo Brother!');
+})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`server listening to port ${PORT}`);
+});
